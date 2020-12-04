f7e48cf684063cd912b31ed53014633c465dc154
ayajmohammad <ayaj@blockgemini.com>
2020-12-04 09:22:17 +0530

    routes

--- server.js
+++ server.js
@@ -2,18 +2,15 @@ var express = require('express');
 var app = express();
 var env = require("./config/"+process.env.NODE_ENV)
 const port = env.port;
-
 var routers = require('./src/routes/index');
-//Initiallize Route
+// Initiallize Route
 routers.initialize(app);
-// helloc 22
 app.get('/environment',(req,res)=>{
     res.send({
         code:200,
         msg:process.env.NODE_ENV
     })
 })
-// djsoijvio
 app.listen(port,() => {
     console.log("server running on"+"--"+process.env.NODE_ENV);
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,6 @@ const user = require('./user')
 
 
 module.exports.initialize = function (app) {
- console.log("hello");
- console.log("====",app)
     /* Employee */
     app.use('/emp', user)
 
